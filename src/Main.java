40da3d396f392aeb34ddb180ee4a50e6fe1c1a51
Lexi Ross <the.circ@gmail.com>
2012-04-26 06:08:35 -0400

    lol good morning

--- src/Main.java
+++ src/Main.java
@@ -49,66 +49,195 @@ public class Main {
 		return A;
 	}
 
-	private static void runAll(long[] A, int max) {
-		long start = System.nanoTime();
-		long kkResidue = LocalSearch.kk(A);
-		long elapsed = System.nanoTime() - start;
-		double seconds = (double)elapsed / 1000000000.0;
-		System.out.println("\nKK: " + kkResidue + "\tTime: " + seconds);
+	// The monstrous testing method
+	private static void runAll(int max, int n) {
+		long[] kkResidues = new long[n];
+		long[] standardrrResidues = new long[n];
+		long[] standardhcResidues = new long[n];
+		long[] standardsaResidues = new long[n];
+		long[] pprrResidues = new long[n];
+		long[] pphcResidues = new long[n];
+		long[] ppsaResidues = new long[n];
+		double[] kkTimes = new double[n];
+		double[] standardrrTimes = new double[n];
+		double[] standardhcTimes = new double[n];
+		double[] standardsaTimes = new double[n];
+		double[] pprrTimes = new double[n];
+		double[] pphcTimes = new double[n];
+		double[] ppsaTimes = new double[n];
+		for (int i = 0; i < n; i++) {
+			long[] A = generateProblem(100, 1000000000000L);
+			long start = System.nanoTime();
+			long kkResidue = LocalSearch.kk(A);
+			kkResidues[i] = kkResidue;
+			long elapsed = System.nanoTime() - start;
+			double seconds = (double)elapsed / 1000000000.0;
+			kkTimes[i] = seconds;
+			System.out.println("\nKK: " + kkResidue + "\tTime: " + seconds);
+
+			Solution standard = new StandardSolution(A.length);		
+			System.out.println("\nStandard Representation:");
+			start = System.nanoTime();
+			long rrResidue = LocalSearch.repeatedRandom(A, standard, max);
+			standardrrResidues[i] = rrResidue;
+			elapsed = System.nanoTime() - start;
+			seconds = (double)elapsed / 1000000000.0;
+			standardrrTimes[i] = seconds;
+			System.out.println("Repeated Random: " + rrResidue + "\tTime: " + seconds);
+			start = System.nanoTime();
+			long hcResidue = LocalSearch.hillClimbing(A, standard, max);
+			standardhcResidues[i] = hcResidue;
+			elapsed = System.nanoTime() - start;
+			seconds = (double)elapsed / 1000000000.0;
+			standardhcTimes[i] = seconds;
+			System.out.println("Hill Climbing: " + hcResidue + "\tTime: " + seconds);
+			start = System.nanoTime();
+			long saResidue = LocalSearch.simulatedAnnealing(A, standard, max);
+			standardsaResidues[i] = saResidue;
+			elapsed = System.nanoTime() - start;
+			seconds = (double)elapsed / 1000000000.0;
+			standardsaTimes[i] = seconds;
+			System.out.println("Simulated Annealing: " + saResidue + "\tTime: " + seconds);
+
+			Solution prepartition = new PrepartitionSolution(A.length);		
+			System.out.println("\nPrepartition Representation:");
+			start = System.nanoTime();
+			rrResidue = LocalSearch.repeatedRandom(A, prepartition, max);
+			pprrResidues[i] = rrResidue;
+			elapsed = System.nanoTime() - start;
+			seconds = (double)elapsed / 1000000000.0;
+			pprrTimes[i] = seconds;
+			System.out.println("Repeated Random: " + rrResidue + "\tTime: " + seconds);
+			start = System.nanoTime();
+			hcResidue = LocalSearch.hillClimbing(A, prepartition, max);
+			pphcResidues[i] = hcResidue;
+			elapsed = System.nanoTime() - start;
+			seconds = (double)elapsed / 1000000000.0;
+			pphcTimes[i] = seconds;
+			System.out.println("Hill Climbing: " + hcResidue + "\tTime: " + seconds);
+			start = System.nanoTime();
+			saResidue = LocalSearch.simulatedAnnealing(A, prepartition, max);
+			ppsaResidues[i] = saResidue;
+			elapsed = System.nanoTime() - start;
+			seconds = (double)elapsed / 1000000000.0;
+			ppsaTimes[i] = seconds;
+			System.out.println("Simulated Annealing: " + saResidue + "\tTime: " + seconds);
+		}
+		System.out.print("{");
+		for (int i = 0; i < n; i++) {
+			if (i == n-1)
+				System.out.print("{" + i + "," + kkResidues[i] + "}},");
+			else
+				System.out.print("{" + i + "," + kkResidues[i] + "},");
+		}
+		System.out.print("{");
+		for (int i = 0; i < n; i++) {
+			if (i == n-1)
+				System.out.print("{" + i + "," + standardrrResidues[i] + "}},");
+			else
+				System.out.print("{" + i + "," + standardrrResidues[i] + "},");
+		}
+		System.out.print("{");
+		for (int i = 0; i < n; i++) {
+			if (i == n-1)
+				System.out.print("{" + i + "," + standardhcResidues[i] + "}},");
+			else
+				System.out.print("{" + i + "," + standardhcResidues[i] + "},");
+		}
+		System.out.print("{");
+		for (int i = 0; i < n; i++) {
+			if (i == n-1)
+				System.out.print("{" + i + "," + standardsaResidues[i] + "}},");
+			else
+				System.out.print("{" + i + "," + standardsaResidues[i] + "},");
+		}
+		System.out.print("{");
+		for (int i = 0; i < n; i++) {
+			if (i == n-1)
+				System.out.print("{" + i + "," + pprrResidues[i] + "}},");
+			else
+				System.out.print("{" + i + "," + pprrResidues[i] + "},");
+		}
+		System.out.print("{");
+		for (int i = 0; i < n; i++) {
+			if (i == n-1)
+				System.out.print("{" + i + "," + pphcResidues[i] + "}},");
+			else
+				System.out.print("{" + i + "," + pphcResidues[i] + "},");
+		}
+		System.out.print("{");
+		for (int i = 0; i < n; i++) {
+			if (i == n-1)
+				System.out.print("{" + i + "," + ppsaResidues[i] + "}}");
+			else
+				System.out.print("{" + i + "," + ppsaResidues[i] + "},");
+		}
 		
-		Solution standard = new StandardSolution(A.length);		
-		System.out.println("\nStandard Representation:");
-		start = System.nanoTime();
-		long rrResidue = LocalSearch.repeatedRandom(A, standard, max);
-		elapsed = System.nanoTime() - start;
-		seconds = (double)elapsed / 1000000000.0;
-		System.out.println("Repeated Random: " + rrResidue + "\tTime: " + seconds);
-		start = System.nanoTime();
-		long hcResidue = LocalSearch.hillClimbing(A, standard, max);
-		elapsed = System.nanoTime() - start;
-		seconds = (double)elapsed / 1000000000.0;
-		System.out.println("Hill Climbing: " + hcResidue + "\tTime: " + seconds);
-		start = System.nanoTime();
-		long saResidue = LocalSearch.simulatedAnnealing(A, standard, max);
-		elapsed = System.nanoTime() - start;
-		seconds = (double)elapsed / 1000000000.0;
-		System.out.println("Simulated Annealing: " + saResidue + "\tTime: " + seconds);
+		System.out.println("Time:");
+		System.out.print("{");
+		for (int i = 0; i < n; i++) {
+			if (i == n-1)
+				System.out.print("{" + i + "," + kkTimes[i] + "}},");
+			else
+				System.out.print("{" + i + "," + kkTimes[i] + "},");
+		}
+		System.out.print("{");
+		for (int i = 0; i < n; i++) {
+			if (i == n-1)
+				System.out.print("{" + i + "," + standardrrTimes[i] + "}},");
+			else
+				System.out.print("{" + i + "," + standardrrTimes[i] + "},");
+		}
+		System.out.print("{");
+		for (int i = 0; i < n; i++) {
+			if (i == n-1)
+				System.out.print("{" + i + "," + standardhcTimes[i] + "}},");
+			else
+				System.out.print("{" + i + "," + standardhcTimes[i] + "},");
+		}
+		System.out.print("{");
+		for (int i = 0; i < n; i++) {
+			if (i == n-1)
+				System.out.print("{" + i + "," + standardsaTimes[i] + "}},");
+			else
+				System.out.print("{" + i + "," + standardsaTimes[i] + "},");
+		}
+		System.out.print("{");
+		for (int i = 0; i < n; i++) {
+			if (i == n-1)
+				System.out.print("{" + i + "," + pprrTimes[i] + "}},");
+			else
+				System.out.print("{" + i + "," + pprrTimes[i] + "},");
+		}
+		System.out.print("{");
+		for (int i = 0; i < n; i++) {
+			if (i == n-1)
+				System.out.print("{" + i + "," + pphcTimes[i] + "}},");
+			else
+				System.out.print("{" + i + "," + pphcTimes[i] + "},");
+		}
+		System.out.print("{");
+		for (int i = 0; i < n; i++) {
+			if (i == n-1)
+				System.out.print("{" + i + "," + ppsaTimes[i] + "}}");
+			else
+				System.out.print("{" + i + "," + ppsaTimes[i] + "},");
+		}
 		
-		Solution prepartition = new PrepartitionSolution(A.length);		
-		System.out.println("\nPrepartition Representation:");
-		start = System.nanoTime();
-		rrResidue = LocalSearch.repeatedRandom(A, prepartition, max);
-		elapsed = System.nanoTime() - start;
-		seconds = (double)elapsed / 1000000000.0;
-		System.out.println("Repeated Random: " + rrResidue + "\tTime: " + seconds);
-		start = System.nanoTime();
-		hcResidue = LocalSearch.hillClimbing(A, prepartition, max);
-		elapsed = System.nanoTime() - start;
-		seconds = (double)elapsed / 1000000000.0;
-		System.out.println("Hill Climbing: " + hcResidue + "\tTime: " + seconds);
-		start = System.nanoTime();
-		saResidue = LocalSearch.simulatedAnnealing(A, prepartition, max);
-		elapsed = System.nanoTime() - start;
-		seconds = (double)elapsed / 1000000000.0;
-		System.out.println("Simulated Annealing: " + saResidue + "\tTime: " + seconds);
 	}
 	
 	private static void test() {
-		long[] p1 = generateProblem(100, 1000000000000L);
-		System.out.print("Problem:\n[");
-		for (int i = 0; i < 50; i++) {
-			System.out.print(" " + p1[i] + " ");
-		}
-		System.out.print("]\n");
-		
-		runAll(p1, 25000);
+
+	
+		runAll(25000, 50);
 	}
 	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
+
 		String usage = "Usage: ./kk <inputfile>";
         if (args.length != 1) {
             System.out.println("Wrong number of arguments.");
@@ -120,7 +249,8 @@ public class Main {
         long[] problem = generateFromFile(filename);
         long residue = LocalSearch.kk(problem);
         System.out.println(residue);
-		
+  
+		// Uncomment below to print Mathematica-ready results
 		//test();
 		
 
